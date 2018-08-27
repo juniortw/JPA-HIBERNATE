577d59392376e465361ef0ce6d6b72653fbc5739
Amauri Junior <junior.tw@hotmail.com>
2018-08-26 22:38:45 -0300

    Mapeando os objetos

--- src/br/com/junior/Aplicação/App.java
+++ src/br/com/junior/Aplicação/App.java
@@ -1,20 +1,62 @@
 package br.com.junior.Aplicação;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.junior.Bean.Pessoa;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Maria", "maria@email.com");
-		Pessoa p2 = new Pessoa(2, "Joao", "Joao@email.com");
-		Pessoa p3 = new Pessoa(3, "Junior", "Junior@email.com");
+		Pessoa p1 = new Pessoa(null, "Maria", "maria@email.com");
+		Pessoa p2 = new Pessoa(null, "Joao", "Joao@email.com");
+		Pessoa p3 = new Pessoa(null, "Junior", "Junior@email.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		//Criar uma Persistencia no Banco
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		//Mapeia as entidades
+		EntityManager em = emf.createEntityManager();
 		
+		Adicionar(p1, em);
+		Adicionar(p2, em);
+		Adicionar(p3, em);
 		
+		Buscar(2, em);
+		
+		Remover(p1, 1, em);
+		
+		emf.close();
+		em.close();
+		
+	}
+
+	private static void Remover(Pessoa p, Integer id, EntityManager em) {
+		//Removendo uma pessoa do banco
+		em.find(Pessoa.class, id);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Removido!");
+	}
+
+	private static void Buscar(Integer id, EntityManager em) {
+		//Buscando uma pessoa do banco
+		Pessoa p = em.find(Pessoa.class, id);
+		
+		System.out.println(p);
+		
+	}
+
+	private static void Adicionar(Pessoa p1, EntityManager em) {
+		//Adicionando uma pessoa no bando
+		em.getTransaction().begin(); 
+		em.persist(p1);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
 	}
 
 }
